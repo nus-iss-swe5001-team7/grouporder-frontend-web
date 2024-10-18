6e2bc2b76ee3e20c5fd40f6fc43eb60d033653c5
Bonnie-TJ <e1335039@u.nus.edu>
2024-10-19 00:07:57 +0800

    Update for Connect to backend

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     devServer: {
-        proxy: 'http://localhost:8765/'
+        proxy: 'http://group-order-lb-621478777.ap-southeast-1.elb.amazonaws.com/'
         //  proxy: 'http://3.211.129.88/'
     },
     configureWebpack: {
