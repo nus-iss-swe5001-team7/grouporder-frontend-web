b65b81fdfea1a846c9901f2f9822639ebe5cdf09
Bonnie-TJ <e1335039@u.nus.edu>
2024-10-21 23:31:45 +0800

    feature/GFO-70-Cloudfront and fix payment timer

--- src/services/api/orderAPI.js
+++ src/services/api/orderAPI.js
@@ -1,24 +1,24 @@
 import Api from "@/services/api/baseAPI";
 
-const Order = '/groupFoodOrdersAPI/groupFoodOrder';
+const Order = '/order-service/groupFoodOrdersAPI/groupFoodOrder';
 
 export default {
     sendOrder: (order) => Api().post(Order, order),
 
-    getGroupOrdersByUserId: (userId) =>  Api().get(`/groupFoodOrdersAPI/groupOrders?parameter=${userId}`),
+    getGroupOrdersByUserId: (userId) =>  Api().get(`/order-service/groupFoodOrdersAPI/groupOrders?parameter=${userId}`),
 
-    updateStatusToSubmittedToRestaurant: (orderId) => Api().put(`/groupFoodOrdersAPI/submittedToRestaurant/${orderId}`),
+    updateStatusToSubmittedToRestaurant: (orderId) => Api().put(`/order-service/groupFoodOrdersAPI/submittedToRestaurant/${orderId}`),
 
-    updateStatusToOrderCancel: (orderId) => Api().put(`/groupFoodOrdersAPI/orderCancel/${orderId}`),
+    updateStatusToOrderCancel: (orderId) => Api().put(`/order-service/groupFoodOrdersAPI/orderCancel/${orderId}`),
 
-    updateStatusToKitchenPreparing: (orderId) => Api().put(`/groupFoodOrdersAPI/kitchenPreparing/${orderId}`),
+    updateStatusToKitchenPreparing: (orderId) => Api().put(`/order-service/groupFoodOrdersAPI/kitchenPreparing/${orderId}`),
 
-    updateStatusToReadyForDelivery: (orderId) => Api().put(`/groupFoodOrdersAPI/readyForDelivery/${orderId}`),
+    updateStatusToReadyForDelivery: (orderId) => Api().put(`/order-service/groupFoodOrdersAPI/readyForDelivery/${orderId}`),
 
     updateStatusToOnDelivery: (orderId) => Api().put(`/deliveryAPI/onDelivered/${orderId}`),
 
     updateStatusToDelivered: (orderId) => Api().put(`/deliveryAPI/delivered/${orderId}`),
 
-    deleteOrder: (orderId) =>  Api().put(`/groupFoodOrdersAPI/deleteOrder?parameter=${orderId}`),
+    deleteOrder: (orderId) =>  Api().put(`/order-service/groupFoodOrdersAPI/deleteOrder?parameter=${orderId}`),
 
 }
--- src/services/api/paymentAPI.js
+++ src/services/api/paymentAPI.js
@@ -1,6 +1,6 @@
 import Api from "@/services/api/baseAPI";
 
-const PAYMENT = 'order-service/UpdatePaymentStatusAPI/updatePayment';
+const PAYMENT = '/order-service/UpdatePaymentStatusAPI/updatePayment';
 
 export default {
     updatePaymentStatus: (paymentDetails) => Api().put(PAYMENT, paymentDetails)
